d8f5afd202ba77e866916d84aebf1b665755ae28
Çağrı <124529286+cagrikandemir@users.noreply.github.com>
2024-10-29 16:53:43 +0300

    AddMemberFinito

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import Room from './companents/Roombar/Room'
 import Navi from './companents/navbar/Navi';
 import { Route, Routes } from 'react-router-dom';
+import PopForm from './companents/Roombar/PopForm';
+
 
 function App() {
   return (
     <Routes>
       <Route path='/navi' element={<Navi />} />
       <Route path='/room' element={<Room />} />
-      <Route path='/' element={"Hello World !abs!"} />
+      <Route path='/' element={"Hello World !!"} />
+      <Route path='/pops' element={<PopForm/>}/>
     </Routes>
   );
 
--- src/companents/navbar/Navi.js
+++ src/companents/navbar/Navi.js
@@ -1,37 +1,44 @@
-import React, { useState } from 'react';
-import {Navbar,NavbarBrand,ListGroup,ListGroupItem,Card, CardBody,Button} from 'reactstrap';
-import './Navbar.css';
+import React, {  } from "react";
+import {
+  Navbar,
+  NavbarBrand,
+  ListGroup,
+  ListGroupItem,
+  Card,
+  CardBody,
+  Button,
+} from "reactstrap";
+import "./Navbar.css";
 
-var rooms =[
-    {id:1, name:"Toplantı Odası", uyeler:5},
-    {id:1, name:"Toplantı Odası", uyeler:5},
-    {id:1, name:"Toplantı Odası", uyeler:5},
-    {id:1, name:"Toplantı Odası", uyeler:5},
-    {id:1, name:"Toplantı Odası", uyeler:5},
-    {id:1, name:"Toplantı Odası", uyeler:5},
-    {id:2, name:"Agent Odası", uyeler:10}
-
+var rooms = [
+  { id: 1, name: "Toplantı Odası", uyeler: 5 },
+  { id: 1, name: "Toplantı Odası", uyeler: 5 },
+  { id: 1, name: "Toplantı Odası", uyeler: 5 },
+  { id: 1, name: "Toplantı Odası", uyeler: 5 },
+  { id: 1, name: "Toplantı Odası", uyeler: 5 },
+  { id: 1, name: "Toplantı Odası", uyeler: 5 },
+  { id: 2, name: "Agent Odası", uyeler: 10 },
 ];
 
 function Example() {
   return (
-    <div className='main-div'>
-      <Navbar className='navbar'>
+    <div className="main-div">
+      <Navbar className="navbar">
         <NavbarBrand href="/">Shizzo Chat</NavbarBrand>
-        </Navbar>
-        <ListGroup>
-            {rooms.map(room=>(
-                <ListGroupItem key={room.id} className='list-group-item'>
-                    <Card>
-                        <CardBody>
-                            <h5>{room.name}</h5>
-                            <p>Katılımcı Sayısı: {room.uyeler}</p>
-                            <Button>Odaya Katıl</Button>
-                        </CardBody>
-                    </Card>
-                </ListGroupItem>
-            ))}
-        </ListGroup>
+      </Navbar>
+      <ListGroup>
+        {rooms.map((room) => (
+          <ListGroupItem key={room.id} className="list-group-item">
+            <Card>
+              <CardBody>
+                <h5>{room.name}</h5>
+                <p>Katılımcı Sayısı: {room.uyeler}</p>
+                <Button>Odaya Katıl</Button>
+              </CardBody>
+            </Card>
+          </ListGroupItem>
+        ))}
+      </ListGroup>
     </div>
   );
 }
