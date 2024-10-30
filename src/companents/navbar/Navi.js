2f1a4b450bd7fe4ee2be4e84f102ec3310db4f43
Çağrı <124529286+cagrikandemir@users.noreply.github.com>
2024-10-30 12:38:38 +0300

    update

--- src/companents/navbar/Navi.js
+++ src/companents/navbar/Navi.js
@@ -1,46 +1,37 @@
-import React, {  } from "react";
-import {
-  Navbar,
-  NavbarBrand,
-  ListGroup,
-  ListGroupItem,
-  Card,
-  CardBody,
-  Button,
-} from "reactstrap";
-import "./Navbar.css";
+import React, { useState } from 'react';
+import {Navbar,NavbarBrand,ListGroup,ListGroupItem,Card, CardBody,Button} from 'reactstrap';
+import './Navbar.css';
 
-var rooms = [
-  { id: 1, name: "Toplantı Odası", uyeler: 5 },
-  { id: 1, name: "Toplantı Odası", uyeler: 5 },
-  { id: 1, name: "Toplantı Odası", uyeler: 5 },
-  { id: 1, name: "Toplantı Odası", uyeler: 5 },
-  { id: 1, name: "Toplantı Odası", uyeler: 5 },
-  { id: 1, name: "Toplantı Odası", uyeler: 5 },
-  { id: 2, name: "Agent Odası", uyeler: 10 },
+var rooms =[
+    {id:1, name:"Toplantı Odası", uyeler:5},
+    {id:1, name:"Toplantı Odası", uyeler:5},
+    {id:1, name:"Toplantı Odası", uyeler:5},
+    {id:1, name:"Toplantı Odası", uyeler:5},
+    {id:1, name:"Toplantı Odası", uyeler:5},
+    {id:1, name:"Toplantı Odası", uyeler:5},
+    {id:2, name:"Agent Odası", uyeler:10}
+
 ];
 
-function Example() {
+function Navi() {
   return (
-    <div className="main-div">
-      <Navbar className="navbar">
+    <div className='main-div'>
+      <Navbar className='navbar'>
         <NavbarBrand href="/">Shizzo Chat</NavbarBrand>
-      </Navbar>
-      <ListGroup>
-        {rooms.map((room) => (
-          <ListGroupItem key={room.id} className="list-group-item">
-            <Card>
-              <CardBody>
-                <h5>{room.name}</h5>
-                <p>Katılımcı Sayısı: {room.uyeler}</p>
-                <Button>Odaya Katıl</Button>
-              </CardBody>
-            </Card>
-          </ListGroupItem>
-        ))}
-      </ListGroup>
+        </Navbar>
+        <ListGroup>
+            {rooms.map(room=>(
+                <ListGroupItem key={room.id} className='list-group-item'>
+                    <Card>
+                            <h5>{room.name}</h5>
+                            <p>Katılımcı Sayısı: {room.uyeler}</p>
+                            <Button style={{margin:2}} size='sm'>Odaya Katıl</Button>
+                    </Card>
+                </ListGroupItem>
+            ))}
+        </ListGroup>
     </div>
   );
 }
 
-export default Example;
+export default Navi;
